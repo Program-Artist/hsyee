1731a7def62b3b5d439655b02b089f7957172e95
Z <1873893450@qq.com>
2019-11-19 18:10:35 +0800

    初次提交

---
+++ admin/config/main.php
@@ -0,0 +1,91 @@
+<?php
+
+$config = require(ROOT_PATH . "/include/config.php");
+$params = array_merge($config['params'], array('administrator' => array('admin'),));
+$st="";
+
+    $params['roleItem'] = array(
+    array(
+        '班级管理',
+        array(
+            'award_index41' => array('我的班级', 'clubList/index'),
+            'award_index42' => array('我教的班级', 'clubList/index'),
+            'award_index44' => array('2333', 'clubList/index'),
+        ),
+    ),
+    array(
+        '成绩管理',
+        array(
+            'award_index41' => array('录入成绩', 'clubList/index'),
+            'award_index42' => array('查看成绩', 'clubList/index'),
+            'award_index44' => array('2333', 'clubList/index'),
+        ),
+    ),
+    array(
+        '考评管理',
+        array(
+            'award_index41' => array('填写考评', 'clubList/index'),
+            'award_index42' => array('查看考评', 'clubList/index'),
+            'award_index44' => array('2333', 'clubList/index'),
+        ),
+    ),
+     
+  );
+
+
+
+
+$main = array(
+    'basePath' => ROOT_PATH . '/admin',
+    'runtimePath' => ROOT_PATH . '/runtime/admin',
+    'name' => '',
+    'defaultController' => 'index',
+    'components' => array(
+        'db' => $config['components']['db'],
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'info,error, warning'
+                ),
+                array(
+                    'class' => 'CWebLogRoute',
+                    'levels' => 'trace'
+                ),
+            ),
+        ),
+    ),
+    'params' => $params,
+);
+
+return array_merge($config, $main);
+?>
+
+<ul class="sidebar-menu">            
+<li class="treeview">               
+    <a href="#">                    
+        <i class="fa fa-gears"></i> <span>权限控制</span>                    
+        <i class="fa fa-angle-left pull-right"></i>               
+    </a>               
+    <ul class="treeview-menu">                   
+        <li class="treeview">                        
+            <a href="/admin">管理员</a>                        
+            <ul class="treeview-menu">                            
+                <li><a href="/user"><i class="fa fa-circle-o"></i> 后台用户</a></li>                            
+                <li class="treeview">                                
+                    <a href="/admin/role"> <i class="fa fa-circle-o"></i> 权限 <i class="fa fa-angle-left pull-right"></i>
+                    </a>                                
+                    <ul class="treeview-menu">                                    
+                        <li><a href="/admin/route"><i class="fa fa-circle-o"></i> 路由</a></li>
+                        <li><a href="/admin/permission"><i class="fa fa-circle-o"></i> 权限</a></li>
+                        <li><a href="/admin/role"><i class="fa fa-circle-o"></i> 角色</a></li>
+                        <li><a href="/admin/assignment"><i class="fa fa-circle-o"></i> 分配</a></li>
+                        <li><a href="/admin/menu"><i class="fa fa-circle-o"></i> 菜单</a></li>
+                    </ul>                           
+                </li>                        
+            </ul>                    
+        </li>                
+    </ul>            
+    </li>        
+</ul>
